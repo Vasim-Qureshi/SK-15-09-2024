438a1fb268290b666b32f13a95b6868cdca4bbdb
vasim-qureshi <vasimqureshi2000@gmail.com>
2025-05-12 00:34:43 +0530

    Backend/session-4-express-api/project-structure/package-lock.json

--- Backend/session-5-cookies/connect.js
+++ Backend/session-5-cookies/connect.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export const connect = async()=>{
     try {
-        await mongoose.connect('mongodb://localhost:27017/auth');
+        await mongoose.connect('mongodb+srv://Workshop:Workshop123@cluster0.w38h7lp.mongodb.net/Cookies-Manager?retryWrites=true&w=majority&appName=Cluster0');
         console.log('connected...')
     } catch (error) {
         console.log('error while connecting DB',error)
